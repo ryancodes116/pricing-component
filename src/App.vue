5dce87681e0bbf36231719172657054b7866d2ac
Ryan Moulton <ryanmoulton116@gmail.com>
2020-07-07 15:03:00 -0700

    add mobile header section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="app">hello world</div>
+  <div id="app">
+    <img src="@/assets/images/bg-top.svg" class="bg-top" />
+    <Header />
+    <img src="@/assets/images/bg-bottom.svg" class="bg-bottom" />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Header
+  }
 };
 </script>
 
@@ -16,12 +24,52 @@ export default {
   box-sizing: border-box;
 }
 
+img {
+  max-width: 100%;
+}
+
 #app {
+  position: relative;
+  overflow: hidden;
   font-family: "Montserrat", sans-serif;
   font-size: 15px;
+  background-image: linear-gradient(#f7f7ff, #f6f6ff);
   display: flex;
   flex-direction: column;
   justify-content: center;
   min-height: 100vh;
+}
+
+.container {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.bg-top,
+.bg-bottom {
+  position: absolute;
+}
+
+.bg-top {
+  top: -1rem;
+  right: -13rem;
+}
+
+.bg-bottom {
+  display: none;
+}
+
+/* Media Queries */
+@media (min-width: 990px) {
+  .bg-top {
+    top: 0;
+    right: 0;
+  }
+
+  .bg-bottom {
+    display: block;
+    bottom: 0;
+    left: 0;
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="header container">
+    <h1 class="title">Our Pricing</h1>
+    <div class="toggleWrapper">
+      <p class="annually">Annually</p>
+      <!-- Toggle Button -->
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+      <p class="monthly">Monthly</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+.header {
+  z-index: 1;
+  text-align: center;
+}
+
+.title {
+  color: hsl(233, 13%, 49%);
+  letter-spacing: 0.04rem;
+  margin-bottom: 2.5rem;
+}
+
+.toggleWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.toggleWrapper p {
+  color: hsl(234, 14%, 74%);
+  font-weight: 700;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  margin: 0 2rem;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-image: linear-gradient(hsl(236, 72%, 79%), hsl(237, 63%, 64%));
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
